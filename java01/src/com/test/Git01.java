236dd274682054b30863e8248fe2b7ad56e4305e
guanglei1015 <3268188701@qq.com>
2018-12-04 10:44:22 +0800

    two project first

---
+++ java01/src/com/test/Git01.java
@@ -0,0 +1,15 @@
+package com.test;
+public class Git01 {
+
+    public static void main(String[] args) {
+        System.out.println("hi git");
+        System.out.println("============2222222222222");
+        System.out.println("333333333333");
+        tt();
+    }
+    
+    public static void tt(){
+    	
+    	System.out.println("1234567889");
+    }
+}
